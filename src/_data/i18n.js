7115e7c16df91a250cf54ea56ff36f4244da2e2c
Christian Kußowski <c.kussowski@famedly.com>
2026-03-22 12:08:37 +0100

    fix: Ignore empty string translations

--- src/_data/i18n.js
+++ src/_data/i18n.js
@@ -1,35 +1,49 @@
+// @ts-check
+
+/**
+ * @typedef {Record<string, string | Record<string, string>>} LangDict
+ */
+
 import fs from "fs";
 import path from "path";
 
 const baseDir = path.join(process.cwd(), "src", "_data", "i18n");
 
+/**
+ * 
+ * @param {LangDict} lang 
+ * @param {LangDict} translated 
+ */
+function mergeLangDict(lang, translated) {
+    const result = { ...lang };
+    for (const key of Object.keys(lang)) {
+        if (typeof lang[key] === "string") {
+            const translation = translated[key];
+            if (translation !== undefined && translation !== null && translation !== "") {
+                result[key] = translation;
+            }
+        } else if (typeof lang[key] === "object" && typeof translated[key] === "object") {
+            result[key] = mergeLangDict(lang[key], translated[key]);
+        }
+    }
+    return result;
+}
+
 export default function () {
+    /** @type {Record<string, LangDict>} */
     const langs = {};
+
+    /** @type {LangDict} */
     const defaultLang = JSON.parse(fs.readFileSync(path.join(baseDir, 'en.json'), "utf8"));
+
     for (const file of fs.readdirSync(baseDir)) {
         if (file.endsWith(".json")) {
             const code = path.basename(file, ".json");
-            const lang = JSON.parse(fs.readFileSync(path.join(baseDir, file), "utf8"));
-            langs[code] = {
-                ...defaultLang,
-                ...lang,
-            };
-            langs[code].website = {
-                ...defaultLang.website,
-                ...lang.website,
-            };
-            langs[code].faq_questions = {
-                ...defaultLang.faq_questions,
-                ...lang.faq_questions,
-            };
-            langs[code].faq_answers = {
-                ...defaultLang.faq_answers,
-                ...lang.faq_answers,
-            };
 
-            if (code != 'en' && langs[code].website.languageName == 'English') {
-                langs[code] = undefined;
-            }
+            /** @type {LangDict} */
+            let lang = JSON.parse(fs.readFileSync(path.join(baseDir, file), "utf8"));
+
+            langs[code] = mergeLangDict(defaultLang, lang);
         }
     }
     return {
